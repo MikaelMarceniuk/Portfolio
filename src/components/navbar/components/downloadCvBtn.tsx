c72b2e1a380e37efbb8773df01b41caa6d9cad28
Mikael Marceniuk <mika.marceniuk@gmail.com>
2024-03-30 20:18:48 -0300

    feat: Btn do download CV is sending user to CV

--- src/components/navbar/components/downloadCvBtn.tsx
+++ src/components/navbar/components/downloadCvBtn.tsx
@@ -1,12 +1,13 @@
 import { DownloadSimple } from "phosphor-react"
 import { useTheme } from "styled-components"
 import BaseBtnWrapper from "./baseBtnWrapper"
+import Constants from "../../../constants"
 
 const DownloadCvBtn = () => {
 	const { colors } = useTheme()
 
 	return (
-		<BaseBtnWrapper>
+		<BaseBtnWrapper as={'a'} href={Constants.linkToCv} target="_blank">
 			<DownloadSimple
 				color={colors.gray900}
 				size={18} 
--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,8 @@
 const Constants = {
 	email: 'mika.marceniuk@gmail.com',
 	wppNumber: '+55 16 99796-5512',
-	wppLinkToMessageMe: 'https://wa.link/1qmli2'
+	wppLinkToMessageMe: 'https://wa.link/1qmli2',
+	linkToCv: 'https://drive.google.com/file/d/14FulwZnz8-JLYjvcl4pnN9z80SCJBU4p/view'
 } as const
 
 export default Constants
