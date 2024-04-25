88c3d7b2e353e6ad66b60120e5f39b6cb87bfc87
Mikael Marceniuk <mika.marceniuk@gmail.com>
2024-04-25 00:22:52 -0300

    feat(Navbar): Add accordion and animation when closing/opening navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,8 +1,10 @@
 'use client'
 
 import NavbarItems from '@/data/navbarItems'
+import { AccordionItem } from '@radix-ui/react-accordion'
 import { Menu, X } from 'lucide-react'
 import { useState } from 'react'
+import { Accordion, AccordionContent, AccordionTrigger } from '../ui/accordion'
 import ListItem from './_components/listItem'
 
 const Navbar: React.FC = () => {
@@ -12,28 +14,35 @@ const Navbar: React.FC = () => {
 
 	return (
 		<div className='sticky top-0 border-b bg-background px-4'>
-			<div className='flex h-20 items-center justify-end'>
-				<div className='rounded border p-2' onClick={toggleMenuOpen}>
-					<Menu
-						data-active={!isOpen}
-						className='hidden opacity-0 transition-opacity data-[active=true]:block data-[active=true]:opacity-100'
-					/>
-					<X
-						data-active={isOpen}
-						className='hidden opacity-0 transition-opacity data-[active=true]:block data-[active=true]:opacity-100'
-					/>
-				</div>
-			</div>
-			<div
-				data-active={isOpen}
-				className='hidden h-0 p-2 data-[active=true]:block data-[active=true]:h-fit data-[active=true]:pb-4'
-			>
-				<ul className='flex list-none flex-col gap-4'>
-					{NavbarItems.map((item, i) => (
-						<ListItem key={i} {...item} />
-					))}
-				</ul>
-			</div>
+			<Accordion type='single' collapsible>
+				<AccordionItem value='navbar-menu'>
+					<AccordionTrigger className='justify-end'>
+						<div className='rounded border p-2' onClick={toggleMenuOpen}>
+							<Menu
+								data-active={!isOpen}
+								className='hidden opacity-0 transition-opacity data-[active=true]:block data-[active=true]:opacity-100'
+							/>
+							<X
+								data-active={isOpen}
+								className='hidden opacity-0 transition-opacity data-[active=true]:block data-[active=true]:opacity-100'
+							/>
+						</div>
+					</AccordionTrigger>
+
+					<AccordionContent>
+						<div
+							data-active={isOpen}
+							className='hidden h-0 p-2 data-[active=true]:block data-[active=true]:h-fit data-[active=true]:pb-4'
+						>
+							<ul className='flex list-none flex-col gap-4'>
+								{NavbarItems.map((item, i) => (
+									<ListItem key={i} {...item} />
+								))}
+							</ul>
+						</div>
+					</AccordionContent>
+				</AccordionItem>
+			</Accordion>
 		</div>
 	)
 }
